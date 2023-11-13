3373c7f6e7df38ded0e8b07e0d3a60694fbb3d66
skudunter <danielgruttercpt@gmail.com>
2023-11-13 18:01:58 +0200

    feat added bg

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import Board from "./board";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <p className="text-red-600">hello world</p>
+  <main className="flex justify-center items-center h-screen bg-neutral-800">
+    <Board/>
+  </main>
 );
