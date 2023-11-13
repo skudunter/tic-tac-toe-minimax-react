3a0437df5e9e0160099949cefd2ed05bc112f9a0
skudunter <danielgruttercpt@gmail.com>
2023-11-13 19:52:44 +0200

    feat player can now make moves

--- src/board.tsx
+++ src/board.tsx
@@ -1,11 +1,29 @@
 import Cell from "./cell";
+import { useState } from "react";
 const Board = () => {
+  const [isPlayerTurn, setIsPlayerTurn] = useState(true);
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  function doMove(id: number) {
+    if (isPlayerTurn) {
+      if (board[id] != "o") {
+        const tempBoard = board;
+        tempBoard[id] = "o";
+        setBoard(tempBoard);
+        setIsPlayerTurn(!isPlayerTurn);
+      }
+    }
+  }
   return (
-    <div className="grid grid-cols-3  max-w-screen-md mx-auto">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
-        <Cell id={index} />
-      ))}
-    </div>
+    <section>
+      <p className="text-2xl text-white mb-2 text-center">{`${
+        isPlayerTurn ? "🤓" : "🤖"
+      } turn`}</p>
+      <div className="grid grid-cols-3  max-w-screen-md mx-auto">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
+          <Cell id={index - 1} value={board[index - 1]} doMove ={doMove} />
+        ))}
+      </div>
+    </section>
   );
 };
 
--- src/cell.tsx
+++ src/cell.tsx
@@ -1,9 +1,22 @@
-export default function Cell({ id }: { id: number }) {
+export default function Cell({
+  id,
+  value,
+  doMove,
+}: {
+  id: number;
+  value: string;
+  doMove: (id:number) => void;
+}) {
   return (
     <button
+      onClick={() => {
+        doMove(id);
+      }}
       className={`w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 xl:w-40 xl:h-40 ${
         id % 2 === 0 ? "bg-cyan-50" : "bg-slate-950"
       }`}
-    ></button>
+    >
+      {value}
+    </button>
   );
 }
